1329246e65aeca514b09f2716ef2136533385428
Emil Velikov <emil.l.velikov@gmail.com>
2015-03-28 18:23:01 +0000

    android: mesa: fix the path of the SSE4_1 optimisations
    Commit dd6f641303c(mesa: Build with subdir-objects.) removed the SRCDIR
    variable, but forgot to update all references of it.
    
    Cc: "10.5" <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/mesa/Android.libmesa_dricore.mk
+++ src/mesa/Android.libmesa_dricore.mk
@@ -47,8 +47,8 @@ endif # MESA_ENABLE_ASM
 
 ifeq ($(ARCH_X86_HAVE_SSE4_1),true)
 LOCAL_SRC_FILES += \
-	$(SRCDIR)main/streaming-load-memcpy.c \
-	$(SRCDIR)main/sse_minmax.c
+	$(MESA_TOP)/src/mesa/main/streaming-load-memcpy.c \
+	$(MESA_TOP)/src/mesa/main/sse_minmax.c
 LOCAL_CFLAGS := -msse4.1
 endif
 
