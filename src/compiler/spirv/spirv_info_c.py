f0d2d6e06dd583d84621d2a84e0a7f3b12f6d6a7
Ian Romanick <ian.d.romanick@intel.com>
2017-07-14 18:39:03 -0700

    spirv: Generate spirv_info.c
    The old table based spirv_*_to_string functions would return NULL for
    any values "inside" the table that didn't have entries.  The tables also
    needed to be updated by hand each time a new spirv.h was imported.
    Generate the file instead.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Suggested-by: Jason Ekstrand <jason@jlekstrand.net>

---
+++ src/compiler/spirv/spirv_info_c.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2017 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+import json
+import mako.template
+
+def collect_data(spirv, kind):
+    for x in spirv["operand_kinds"]:
+        if x["kind"] == kind:
+            operands = x
+
+    # There are some duplicate values in some of the tables (thanks guys!), so
+    # filter them out.
+    last_value = -1
+    values = []
+    for x in operands["enumerants"]:
+        if x["value"] != last_value:
+            last_value = x["value"]
+            values.append(x["enumerant"])
+
+    return (kind, values)
+
+if __name__ == "__main__":
+    t = mako.template.Template("""/*
+ * Copyright (C) 2017 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "spirv_info.h"
+    % for kind,values in info:
+
+const char *
+spirv_${kind.lower()}_to_string(Spv${kind} v)
+{
+   switch (v) {
+        % for name in values:
+   case Spv${kind}${name}: return "Spv${kind}${name}";
+        % endfor
+   case Spv${kind}Max: break; /* silence warnings about unhandled enums. */
+   }
+
+   return "unknown";
+}
+    % endfor
+""")
+
+    spirv_info = json.JSONDecoder().decode(open("spirv/spirv.core.grammar.json", "r").read())
+
+    capabilities = collect_data(spirv_info, "Capability")
+    decorations = collect_data(spirv_info, "Decoration")
+
+    print(t.render(info=[capabilities, decorations]))
