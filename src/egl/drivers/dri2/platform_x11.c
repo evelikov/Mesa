05a23b5416e844058ff36283a5c8bb20a170a778
Emil Velikov <emil.velikov@collabora.com>
2017-08-01 22:28:02 +0100

    egl: add dri2_setup_swap_interval helper
    The current two implementations - X11 and Wayland were identical,
    barrind the upper limit.
    
    Instead of having same code twice - introduce a helper and pass the
    limit as an argument.
    
    Thus as Android/DRM/others get support - they only need to call the
    function ;-)
    
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    ---
    FWIW I choose to keep the local wrapper since I'm too lazy to copy the
    same comments multiple times. Can fold it if people prefer.

--- src/egl/drivers/dri2/platform_x11.c
+++ src/egl/drivers/dri2/platform_x11.c
@@ -1259,46 +1259,14 @@ dri2_initialize_x11_swrast(_EGLDriver *drv, _EGLDisplay *disp)
 }
 
 static void
-dri2_x11_setup_swap_interval(struct dri2_egl_display *dri2_dpy)
+dri2_x11_setup_swap_interval(_EGLDisplay *disp)
 {
-   GLint vblank_mode = DRI_CONF_VBLANK_DEF_INTERVAL_1;
    int arbitrary_max_interval = 1000;
 
-   /* default behavior for no SwapBuffers support: no vblank syncing
-    * either.
-    */
-   dri2_dpy->min_swap_interval = 0;
-   dri2_dpy->max_swap_interval = 0;
-
    /* If we do have swapbuffers, then we can support pretty much any swap
-    * interval, but we allow driconf to override applications.
+    * interval.
     */
-   if (dri2_dpy->config)
-      dri2_dpy->config->configQueryi(dri2_dpy->dri_screen,
-                                     "vblank_mode", &vblank_mode);
-   switch (vblank_mode) {
-   case DRI_CONF_VBLANK_NEVER:
-      dri2_dpy->min_swap_interval = 0;
-      dri2_dpy->max_swap_interval = 0;
-      dri2_dpy->default_swap_interval = 0;
-      break;
-   case DRI_CONF_VBLANK_ALWAYS_SYNC:
-      dri2_dpy->min_swap_interval = 1;
-      dri2_dpy->max_swap_interval = arbitrary_max_interval;
-      dri2_dpy->default_swap_interval = 1;
-      break;
-   case DRI_CONF_VBLANK_DEF_INTERVAL_0:
-      dri2_dpy->min_swap_interval = 0;
-      dri2_dpy->max_swap_interval = arbitrary_max_interval;
-      dri2_dpy->default_swap_interval = 0;
-      break;
-   default:
-   case DRI_CONF_VBLANK_DEF_INTERVAL_1:
-      dri2_dpy->min_swap_interval = 0;
-      dri2_dpy->max_swap_interval = arbitrary_max_interval;
-      dri2_dpy->default_swap_interval = 1;
-      break;
-   }
+   dri2_setup_swap_interval(disp, arbitrary_max_interval);
 }
 
 #ifdef HAVE_DRI3
@@ -1340,7 +1308,7 @@ dri2_initialize_x11_dri3(_EGLDriver *drv, _EGLDisplay *disp)
 
    dri2_setup_screen(disp);
 
-   dri2_x11_setup_swap_interval(dri2_dpy);
+   dri2_x11_setup_swap_interval(disp);
 
    if (!dri2_dpy->is_different_gpu)
       disp->Extensions.KHR_image_pixmap = EGL_TRUE;
@@ -1420,7 +1388,7 @@ dri2_initialize_x11_dri2(_EGLDriver *drv, _EGLDisplay *disp)
 
    dri2_setup_screen(disp);
 
-   dri2_x11_setup_swap_interval(dri2_dpy);
+   dri2_x11_setup_swap_interval(disp);
 
    disp->Extensions.KHR_image_pixmap = EGL_TRUE;
    disp->Extensions.NOK_swap_region = EGL_TRUE;
