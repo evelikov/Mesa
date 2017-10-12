aras-p/glsl-optimizer

commit a43d286ef7ff65087b1f051d071b829ca7b02073

gbm: Add import from fd
Add a new import type that lets us create a gbm bo from a
DMA-BUF file descriptor.

Signed-off-by: Kristian Høgsberg <krh@bitplanet.net>

src/gbm/backends/dri/gbm_dri.c
@@ -448,7 +448,7 @@ gbm_dri_bo_import(struct gbm_device *gbm,
       if (!wb)
          return NULL;
 
-      image = wb->driver_buffer;
+      image = dri->image->dupImage(wb->driver_buffer, NULL);
 
       switch (wb->format) {
       case WL_DRM_FORMAT_XRGB8888:
@@ -477,10 +477,27 @@ gbm_dri_bo_import(struct gbm_device *gbm,
          return NULL;
 
       image = dri->lookup_image(dri->screen, buffer, dri->lookup_user_data);
+      image = dri->image->dupImage(image, NULL);
       dri->image->queryImage(image, __DRI_IMAGE_ATTRIB_FORMAT, &dri_format);
       gbm_format = gbm_dri_to_gbm_format(dri_format);
       if (gbm_format == 0)
          return NULL;
+      break;
+   }
+
+   case GBM_BO_IMPORT_FD:
+   {
+      struct gbm_import_fd_data *fd_data = buffer;
+      int stride = fd_data->stride, offset = 0;
+
+      image = dri->image->createImageFromFds(dri->screen,
+                                             fd_data->width,
+                                             fd_data->height,
+                                             fd_data->format,
+                                             &fd_data->fd, 1,
+                                             &stride, &offset,
+                                             NULL);
+      gbm_format = fd_data->format;
       break;
    }
 
@@ -493,7 +510,7 @@ gbm_dri_bo_import(struct gbm_device *gbm,
    if (bo == NULL)
       return NULL;
 
-   bo->image = dri->image->dupImage(image, NULL);
+   bo->image = image;
 
    if (usage & GBM_BO_USE_SCANOUT)
       dri_use |= __DRI_IMAGE_USE_SCANOUT;

src/gbm/main/gbm.c
@@ -362,6 +362,7 @@ gbm_bo_create(struct gbm_device *gbm,
  *
  *   GBM_BO_IMPORT_WL_BUFFER
  *   GBM_BO_IMPORT_EGL_IMAGE
+ *   GBM_BO_IMPORT_FD
  *
  * The the gbm bo shares the underlying pixels but its life-time is
  * independent of the foreign object.

src/gbm/main/gbm.h
@@ -232,6 +232,15 @@ gbm_bo_create(struct gbm_device *gbm,
 
 #define GBM_BO_IMPORT_WL_BUFFER         0x5501
 #define GBM_BO_IMPORT_EGL_IMAGE         0x5502
+#define GBM_BO_IMPORT_FD                0x5503
+
+struct gbm_import_fd_data {
+   int fd;
+   uint32_t width;
+   uint32_t height;
+   uint32_t stride;
+   uint32_t format;
+};
 
 struct gbm_bo *
 gbm_bo_import(struct gbm_device *gbm, uint32_t type,

evelikov/Mesa

commit 586f25ffb9c839438db98a11130067a7a58d1e9f

gbm: handle queryImage() failure for GBM_BO_IMPORT_EGL_IMAGE
The function can fail. Check and teardown accordingly.

Fixes: a43d286ef7f ("gbm: Add import from fd")
Cc: Kristian Høgsberg <krh@bitplanet.net>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/gbm/backends/dri/gbm_dri.c
@@ -891,6 +891,7 @@ gbm_dri_bo_import(struct gbm_device *gbm,
    __DRIimage *image;
    unsigned dri_use = 0;
    int gbm_format;
+   unsigned query; /* EGLBoolean, but we cannot include the header */
 
    /* Required for query image WIDTH & HEIGHT */
    if (dri->image == NULL || dri->image->base.version < 4) {
@@ -934,7 +935,12 @@ gbm_dri_bo_import(struct gbm_device *gbm,
 
       image = dri->lookup_image(dri->screen, buffer, dri->lookup_user_data);
       image = dri->image->dupImage(image, NULL);
-      dri->image->queryImage(image, __DRI_IMAGE_ATTRIB_FORMAT, &dri_format);
+      query = dri->image->queryImage(image, __DRI_IMAGE_ATTRIB_FORMAT, &dri_format);
+      if (!query) {
+         errno = EINVAL;
+         dri->image->destroyImage(image);
+         break;
+      }
       gbm_format = gbm_dri_to_gbm_format(dri_format);
       if (gbm_format == 0) {
          errno = EINVAL;