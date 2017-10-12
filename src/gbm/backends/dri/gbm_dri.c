aras-p/glsl-optimizer

commit 93ebec87ed46978942464a6f08801ae06e86fa8b

dri: Make query image WIDTH and HEIGHT be version 4
Tested-by: Scott Moreau <oreaus@gmail.com>
Signed-off-by: Jakob Bornecrantz <jakob@vmware.com>

src/gbm/backends/dri/gbm_dri.c
@@ -365,6 +365,10 @@ gbm_dri_bo_import(struct gbm_device *gbm,
    unsigned dri_use = 0;
    int dri_format, width, height, gbm_format, stride, cpp, offset;
 
+   /* Required for query image WIDTH & HEIGHT */
+   if (dri->image->base.version < 4)
+      return NULL;
+
    switch (type) {
 #if HAVE_WAYLAND_PLATFORM
    case GBM_BO_IMPORT_WL_BUFFER:

include/GL/internal/dri_interface.h
@@ -945,7 +945,7 @@ struct __DRIdri2ExtensionRec {
 #define __DRI_IMAGE_ATTRIB_HANDLE	0x2001
 #define __DRI_IMAGE_ATTRIB_NAME		0x2002
 #define __DRI_IMAGE_ATTRIB_FORMAT	0x2003 /* available in versions 3+ */
-#define __DRI_IMAGE_ATTRIB_WIDTH	0x2004 /* available in versions 5+ */
+#define __DRI_IMAGE_ATTRIB_WIDTH	0x2004 /* available in versions 4+ */
 #define __DRI_IMAGE_ATTRIB_HEIGHT	0x2005
 
 typedef struct __DRIimageRec          __DRIimage;

evelikov/Mesa

commit bb828f3662b7503677c41a0214fafc4e335cc339

gbm: remove incorrect DRIImage version check in gbm_dri_bo_import()
The version of the extension does not imply success for the
queryImage(... WIDTH/HEIGHT ...) calls.

With that properly handled (as of last commit), we can drop the check.

Fixes: 93ebec87ed4 ("dri: Make query image WIDTH and HEIGHT be version 4")
Cc: Jakob Bornecrantz <jakob.bornecrantz@collabora.co.uk>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/gbm/backends/dri/gbm_dri.c
@@ -893,8 +893,7 @@ gbm_dri_bo_import(struct gbm_device *gbm,
    int gbm_format;
    unsigned query; /* EGLBoolean, but we cannot include the header */
 
-   /* Required for query image WIDTH & HEIGHT */
-   if (dri->image == NULL || dri->image->base.version < 4) {
+   if (dri->image == NULL) {
       errno = ENOSYS;
       return NULL;
    }